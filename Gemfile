0cc3cee24e1bde6111f7606259999a16eb67e65b
Doris Morales <moralesdoris880@gmail.com>
2022-02-18 17:59:37 -0500

    commit tree 0e7f1ac31ab3076d35247c9e76ad882738f1b653

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source "https://rubygems.org"
 
 
-# Scraping
-gem 'nokogiri'
+
 
 # Database
 gem 'sqlite3', '~>1.3.6'
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+    attr_accessor :id,:name,:type,:db
+    def initialize(id:,name:,type:,db:)
+        @id=id
+        @name=name
+        @type=type
+        @db=db
+    end
+    def self.save(name,type,db)
+        db.execute("INSERT INTO pokemon(name,type) VALUES(?,?)",name,type)
+    end
+    def self.find(id,db)
+        poke= db.execute("SELECT * FROM pokemon WHERE id=?",id).flatten
+        Pokemon.new(id:poke[0],name:poke[1],type:poke[2],db:db)
+    end
 end
